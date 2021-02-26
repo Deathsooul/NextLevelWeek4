3193ecaf52cff52e8bdeece07be08c7bf38b5af2
Wallace <wbescrovaine@gmail.com>
2021-02-25 23:41:22 -0300

    Nao sei por que nao funciona o css desse krl

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,20 +1,27 @@
-import Head from 'next/head'
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className= 'container'>
+    <div className={styles.container}>
+      <ExperienceBar />
 
-      <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
-      </Head>
+      
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+        </div>
+        <div>
 
+        </div>
+       
+      </section>
 
-
-
-      <ExperienceBar/>
     </div>
   )
 }
- 
+  
